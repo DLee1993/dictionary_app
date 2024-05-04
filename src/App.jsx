be1a233fa97746d8725794a8b27622ab90ed8a61
David Lee <Dai180293@gmail.com>
2024-05-04 07:27:18 +0100

    re-structured content layout

--- src/App.jsx
+++ src/App.jsx
@@ -72,18 +72,27 @@ function App() {
         <SkeletonTheme>
             <section className="py-10 font-sans">
                 <header className="flex justify-between items-center">
-                    <a href="/dictionary_app/">
-                        <h1 className="text-lg md:text-xl font-bold uppercase text-dark">
-                            wordup
-                        </h1>
+                    <a href="/dictionary_app/" className="relative">
+                        <h1 className="relative z-50 text-lg md:text-xl font-bold uppercase text-dark">wordup</h1>
+                        <div className="absolute top-0 left-0 w-2/5 h-full bg-accent"></div>
                     </a>
                     <a href="https://github.com/DLee1993" target="_blank">
                         <p className="sr-only">github profile</p> <Github />
                     </a>
                 </header>
                 <main className="mt-16 m-auto max-w-3xl">
+                    <section className="mt-5 md:mt-10 mb-10">
+                        <div className="font-bold mx-auto text-center space-y-8" id="startMessage">
+                            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-dark/50">
+                                Level up your knowledge of the english language.
+                            </h1>
+                            <p className="text-sm">
+                                To get started, type a word in the search bar and press enter.
+                            </p>
+                        </div>
+                    </section>
                     <section>
-                        <fieldset className="min-h-20">
+                        <fieldset>
                             <form onSubmit={wordSearch}>
                                 <fieldset className="flex gap-x-2">
                                     <Input
@@ -137,124 +146,105 @@ function App() {
                             <p className="text-accent font-bold mt-1">{inputError}</p>
                         </fieldset>
                     </section>
-                    <section className="mt-5 md:mt-10">
-                        <div
-                            className="font-bold max-w-lg mx-auto text-center space-y-8"
-                            id="startMessage"
-                        >
-                            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-dark/50">
-                                Level up your knowledge of the english language.
-                            </h1>
-                            <p className="text-sm">
-                                To get started, type a word in the search bar and press enter.
-                            </p>
-                        </div>
-                        <section>
-                            <section className="mt-5 flex flex-col justify-between">
-                                <section>
-                                    <section className="flex justify-between">
-                                        <article className="min-w-72">
-                                            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold capitalize">
-                                                {searchResults?.word ||
-                                                    searchError?.title ||
-                                                    (loader && <Skeleton />)}
-                                            </h1>
-                                            <p className="text-accent mt-2 sm:mt-3 md:mt-4">
-                                                {searchResults?.phonetic ||
-                                                    (loader && !searchError && <Skeleton />)}
-                                            </p>
-                                        </article>
-                                        {searchResults?.audio ? (
-                                            <aside className="h-fit">
-                                                <button
-                                                    id="audioControl"
-                                                    className="grid place-items-center size-10 group"
-                                                    onClick={playAudio}
-                                                    disabled={!searchResults?.audio}
-                                                >
-                                                    {audioIcon}
-                                                </button>
-                                                <audio
-                                                    src={searchResults?.audio}
-                                                    id="audio"
-                                                    onPlay={() => IconSwitch(true)}
-                                                    onPause={() => IconSwitch(false)}
-                                                ></audio>
-                                            </aside>
-                                        ) : (
-                                            loader && !searchError && <Skeleton />
-                                        )}
-                                    </section>
-                                    {searchResults?.source ? (
-                                        <p className="flex justify-start items-center flex-wrap gap-x-2 py-5 text-dark/50 dark:text-light/40 font-bold">
-                                            Source:
-                                            <span className="text-dark dark:text-accent font-normal">
-                                                <a
-                                                    className="hover:underline"
-                                                    href={searchResults?.source}
-                                                    target="_blank"
-                                                    aria-label={`wiktionary link for ${searchResults?.word}`}
-                                                >
-                                                    {searchResults?.source}
-                                                </a>
-                                            </span>
+                    <section>
+                        <section className="mt-10 flex flex-col justify-between">
+                            <section>
+                                <section className="flex justify-between">
+                                    <article className="min-w-72">
+                                        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold capitalize">
+                                            {searchResults?.word ||
+                                                searchError?.title ||
+                                                (loader && <Skeleton />)}
+                                        </h1>
+                                        <p className="text-accent mt-2 sm:mt-3 md:mt-4">
+                                            {searchResults?.phonetic ||
+                                                (loader && !searchError && <Skeleton />)}
                                         </p>
+                                    </article>
+                                    {searchResults?.audio ? (
+                                        <aside className="h-fit">
+                                            <button
+                                                id="audioControl"
+                                                className="grid place-items-center size-10 group"
+                                                onClick={playAudio}
+                                                disabled={!searchResults?.audio}
+                                            >
+                                                {audioIcon}
+                                            </button>
+                                            <audio
+                                                src={searchResults?.audio}
+                                                id="audio"
+                                                onPlay={() => IconSwitch(true)}
+                                                onPause={() => IconSwitch(false)}
+                                            ></audio>
+                                        </aside>
                                     ) : (
                                         loader && !searchError && <Skeleton />
                                     )}
                                 </section>
+                                {searchResults?.source ? (
+                                    <p className="flex justify-start items-center flex-wrap gap-x-2 py-5 text-dark/50 dark:text-light/40 font-bold">
+                                        Source:
+                                        <span className="text-dark dark:text-accent font-normal">
+                                            <a
+                                                className="hover:underline"
+                                                href={searchResults?.source}
+                                                target="_blank"
+                                                aria-label={`wiktionary link for ${searchResults?.word}`}
+                                            >
+                                                {searchResults?.source}
+                                            </a>
+                                        </span>
+                                    </p>
+                                ) : (
+                                    loader && !searchError && <Skeleton />
+                                )}
                             </section>
-                            <section>
-                                {searchResults
-                                    ? searchResults?.meanings.map((meaning, index) => (
-                                          <article
-                                              key={index}
-                                              className={index !== 0 ? "py-10" : null}
-                                          >
-                                              <h2 className="flex justify-center items-center gap-x-4 italic font-bold text-xl font-serif">
-                                                  {meaning.partOfSpeech}{" "}
-                                                  <div className="bg-grey/10 w-full h-[2px]"></div>
-                                              </h2>
-                                              <p className="mt-5 font-bold">Meaning</p>
-                                              <ul className="ml-10 mt-10 space-y-2 list-disc">
-                                                  {meaning.definitions.map((def, index) => (
-                                                      <li
-                                                          className="marker:text-accent"
-                                                          key={index}
-                                                      >
-                                                          <div className="flex flex-col gap-y-2">
-                                                              {def.definition}
-                                                              {def.example && (
-                                                                  <span>{`"${def.example}"`}</span>
-                                                              )}
-                                                          </div>
-                                                      </li>
+                        </section>
+                        <section>
+                            {searchResults
+                                ? searchResults?.meanings.map((meaning, index) => (
+                                      <article key={index} className={index !== 0 ? "py-10" : null}>
+                                          <h2 className="flex justify-center items-center gap-x-4 italic font-bold text-xl font-serif">
+                                              {meaning.partOfSpeech}{" "}
+                                              <div className="bg-grey/10 w-full h-[2px]"></div>
+                                          </h2>
+                                          <p className="mt-5 font-bold">Meaning</p>
+                                          <ul className="ml-10 mt-10 space-y-2 list-disc">
+                                              {meaning.definitions.map((def, index) => (
+                                                  <li className="marker:text-accent" key={index}>
+                                                      <div className="flex flex-col gap-y-2">
+                                                          {def.definition}
+                                                          {def.example && (
+                                                              <span>{`"${def.example}"`}</span>
+                                                          )}
+                                                      </div>
+                                                  </li>
+                                              ))}
+                                          </ul>
+                                          {meaning.antonyms?.length > 0 && (
+                                              <aside className="font-bold mt-10">
+                                                  Antonyms:{" "}
+                                                  {meaning.antonyms.map((ant, index) => (
+                                                      <span key={index} className="text-accent">
+                                                          {(index ? ", " : "") + ant}
+                                                      </span>
                                                   ))}
-                                              </ul>
-                                              {meaning.antonyms?.length > 0 && (
-                                                  <aside className="font-bold mt-10">
-                                                      Antonyms:{" "}
-                                                      {meaning.antonyms.map((ant, index) => (
-                                                          <span key={index} className="text-accent">
-                                                              {(index ? ", " : "") + ant}
-                                                          </span>
-                                                      ))}
-                                                  </aside>
-                                              )}
-                                              {meaning.synonyms?.length > 0 && (
-                                                  <aside className="font-bold mt-10">
-                                                      Synonyms:{" "}
-                                                      {meaning.synonyms.map((syn, index) => (
-                                                          <span key={index} className="text-accent">
-                                                              {(index ? ", " : "") + syn}
-                                                          </span>
-                                                      ))}
-                                                  </aside>
-                                              )}
-                                          </article>
-                                      ))
-                                    : loader && !searchError && <Skeleton />}
-                            </section>
+                                              </aside>
+                                          )}
+                                          {meaning.synonyms?.length > 0 && (
+                                              <aside className="font-bold mt-10">
+                                                  Synonyms:{" "}
+                                                  {meaning.synonyms.map((syn, index) => (
+                                                      <span key={index} className="text-accent">
+                                                          {(index ? ", " : "") + syn}
+                                                      </span>
+                                                  ))}
+                                              </aside>
+                                          )}
+                                      </article>
+                                  ))
+                                : loader && !searchError && <Skeleton />}
                         </section>
                     </section>
                 </main>
